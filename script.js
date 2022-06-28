d68c9a2ee0eaa6198152dfc0f9f71df50ca258d8
Kristijan Baro <baro.kristijan@gmail.com>
2022-06-28 20:36:23 +0200

    jednostavni kalkulator inflacije.

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-alert('Pocetak')
+function inflationCalculator() {
+
+    // parseFloat sluzi za pretvaranje stringa u broj sa zarezom.
+    let inflationRate = document.querySelector('#inflationRate');
+    inflationRate = parseFloat(inflationRate.value);
+
+    let money = document.querySelector('#money')
+    money = parseFloat(money.value);
+    
+    let years = document.querySelector('#years').value;
+    years = parseFloat(years);
+
+    // formula za izracunavanje inflacije.
+    let worth = money + (money * (inflationRate / 100));
+
+
+    for(let i = 1; i < years; i++) {
+        worth += worth * (inflationRate / 100);
+    }
+
+    worth = worth.toFixed(2);
+
+    let newElement = document.createElement('div');
+    newElement.className = 'new-value';
+    newElement.innerText = `Danasnjih ${money} € vrijedi isto kao ${worth} € za ${years} godina.`;
+    document.querySelector('.container').appendChild(newElement);
+}
